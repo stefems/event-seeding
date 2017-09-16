43f4fa74b49098cdbc6df104a51816a163458401
stefems <kowalmax.s@gmail.com>
2017-09-16 14:55:29 -0400

    google based event acquisition

--- eventsController.js
+++ eventsController.js
@@ -44,43 +44,107 @@ firebase.auth().signInWithEmailAndPassword("kowalmax.s@gmail.com", "testtest").c
 
 //_________________________________
 //ROUTES___________________________
-router.get('/events/:lat/:lng', (req, res) => {
-	res.send('working');
-	console.log("working");
-	// var options = {        
-	//     url: 'https://api.yelp.com/v3/businesses/search?term=music&latitude=39.743805699999996&longitude=-104.9628995',
-	//     method: 'GET',
-	//     headers:{
-	//         Authorization: ' Bearer cUyNgaY2GZCVViu4jW0Eqsm66z0M4VdD9rV-aiVuzA3KFDjJhoQlB8fZ_R2MHHPD7b7FjPZkB7nmUGNkdtiMSIXV5KK7kbSmOPzKMCVq_YdgBf6SNijCJb30jo2YWXYx'            
-	//    }
-	// };
+router.get('/events/:lat/:long', (req, res) => {
+	//1. all events.orderByChild('lat')
+	/*var ref = firebase.database().ref("events/");
+	ref.once("value")
+	  .then(function(snapshot) {
+	    res.json(snapshot.val());
+	  });
+	*/
+	// TODO: create a query to get all events that within a certain lat/long range of the lat/long passed in.
+	//2. if the number of events we get from this query is small, we should perform a search to get more events in our DB.
+	getMoreEventsAtLocation(req.params.lat, req.params.long, req.query, res);
+});
 
-	// request(options, function(error, response, body) {
-	// 	if (error) {
-	// 		console.log(error);
-	// 	}
-	// 	else if (JSON.parse(body)){
-	// 		JSON.parse(body).businesses.forEach(function(business) {
-	// 			getBusinessWebsiteFromYelp(business.url, business.name);
-	// 		});
-	// 	}
-	// 	else {
-	// 		console.log("failed to hit the yelp api");
-	// 	}
-	// });
-	// request(options, function(error, response, body) {
-	// 	if (error) {
-	// 		console.log("error sending a request to the yelp api.");
-	// 	}
-	// 	else if (JSON.parse(body)){
-	// 		JSON.parse(body).businesses.forEach(function(business) {
-	// 			getBusinessWebsiteFromYelp(business.url, business.name);
-	// 		});
-	// 	}
-	// 	else {
-	// 		console.log("failed to hit the yelp api");
-	// 	}
-	// });
+function getMoreEventsAtLocation(lat, long, queryData, res) {
+	//when all of the promises returned by the function below are done we'll operate on the data.
+	Promise.all(makeGMapsNearbyRequestPromises(lat, long, queryData)).then(function(results){
+		//2D Array, each row is an event type and each column is a place that might have events
+		//Need to get place details for each place in order to get the website
+		results.forEach( (placeList) => {
+			let timeoutDelay = 250;
+			placeList.forEach((place) => {
+				setTimeout(getPlaceDetails, timeoutDelay, place);
+				timeoutDelay += 250;
+			});
+		});
+	    res.json(results);
+	});
+
+	//Send a request to get maps data for each event type
+	function makeGMapsNearbyRequestPromises(lat, long, queryData) {
+		let promises = [];
+		for (var eventType in queryData.types) {
+			if (queryData.types[eventType] === "true") {
+				//push the request promise into an array
+				promises.push(makeOneGMapsNearbyRequest(lat, long, queryData, eventType));
+			}
+		}
+		return promises;
+
+		//makes a promise that only resolves once all of the cursored data is acquired
+		function makeOneGMapsNearbyRequest(lat, long, queryData, eventType) {
+			var promise = new Promise((resolve, reject) => {
+				getAllGmapsDataFromRequest(lat, long, queryData, eventType).then( (results) => {
+					resolve(results);
+				});
+			});
+			return promise;
+
+			function getAllGmapsDataFromRequest(lat, long, queryData, eventType) {
+				var promise = new Promise((resolve, reject) => {
+					
+					//recursively calls itself in order to get cursored data from first request and will
+					//  eventually resolve with all of the cursored data.
+					cursorGMapsRequest(lat, long, queryData, eventType);
+
+					function cursorGMapsRequest(lat, long, queryData, eventType, url, results) {
+						results = results || [];
+						url = url || "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + lat + "," + long +
+						"&radius=" + queryData.radius + "&keyword=" + eventType + "&key=AIzaSyAC4bVTmV7z7rkLOVmXgxlRBsuYOqaAvQU";
+						request(url, (error, response, body) => {
+							body = JSON.parse(body);
+							if (!error && response.statusCode === 200) {
+								results = results.concat(body.results);
+								if (body.next_page_token) {
+									url = url.split("&pagetoken=")[0] + "&pagetoken=" + body.next_page_token;
+									//next_page_token is not active immediately, we must wait a moment.
+									setTimeout(cursorGMapsRequest, 2000, lat, long, queryData, eventType, url, results);
+								}
+								else {
+									resolve(results);
+								}
+								
+							}
+							else {
+								console.log(error);
+								resolve(results);
+							}
+						});
+					}
+				});
+
+				return promise;
+			}
+		}
+	}
+
+	function getPlaceDetails(place) {
+		let url = "https://maps.googleapis.com/maps/api/place/details/json?placeid=" + place.place_id + "&key=AIzaSyAC4bVTmV7z7rkLOVmXgxlRBsuYOqaAvQU"
+		request(url, function(error, response, body) {
+			if (!error && response.statusCode === 200) {
+				body = JSON.parse(body);
+				if (body.result.website && (body.result.website !== "undefined")) {
+					getFacebookPageFromWebsite(body.result.website);
+				}
+			}
+			else {
+				console.log(error);
+			}
+		});
+	}
+}
 
 	function getBusinessWebsiteFromYelp(yelpURL, businessName) {
 		var options = {
@@ -190,7 +254,8 @@ router.get('/events/:lat/:lng', (req, res) => {
 						host: event.place.name,
 						start_time: event.start_time,
 						category: event.category,
-						description: event.description
+						description: event.description,
+						image: event.cover
 					};
 					request.post({
 			            url: 'http://localhost:3000/events/' + event.place.id,
@@ -211,14 +276,14 @@ router.get('/events/:lat/:lng', (req, res) => {
 			}
 		});
 	}
-});
+
 
 router.post('/events/:owner', (req, res) => {
 	if (req.body) {
 		let event = req.body;
 		//CREATE ONE EVENT
 		let tags = [];
-		let setting = database.ref("events/" + req.params.owner + '/eventListing/' + event.facebook_id).set(event);
+		let setting = database.ref("event_sources/" + event.host + "@" + req.params.owner + '/eventListing/' + event.name + "@" + event.facebook_id).set(event);
 		setting.then(function() {
 			res.send({"status": "success", "message": "saved event to firebase.", "event": event});
 		}).catch(function() {
@@ -233,3 +298,37 @@ router.post('/events/:owner', (req, res) => {
 });
 
 module.exports = router;
+	// var options = {        
+	//     url: 'https://api.yelp.com/v3/businesses/search?term=music&latitude=39.743805699999996&longitude=-104.9628995',
+	//     method: 'GET',
+	//     headers:{
+	//         Authorization: ' Bearer cUyNgaY2GZCVViu4jW0Eqsm66z0M4VdD9rV-aiVuzA3KFDjJhoQlB8fZ_R2MHHPD7b7FjPZkB7nmUGNkdtiMSIXV5KK7kbSmOPzKMCVq_YdgBf6SNijCJb30jo2YWXYx'            
+	//    }
+	// };
+
+	// request(options, function(error, response, body) {
+	// 	if (error) {
+	// 		console.log(error);
+	// 	}
+	// 	else if (JSON.parse(body)){
+	// 		JSON.parse(body).businesses.forEach(function(business) {
+	// 			getBusinessWebsiteFromYelp(business.url, business.name);
+	// 		});
+	// 	}
+	// 	else {
+	// 		console.log("failed to hit the yelp api");
+	// 	}
+	// });
+	// request(options, function(error, response, body) {
+	// 	if (error) {
+	// 		console.log("error sending a request to the yelp api.");
+	// 	}
+	// 	else if (JSON.parse(body)){
+	// 		JSON.parse(body).businesses.forEach(function(business) {
+	// 			getBusinessWebsiteFromYelp(business.url, business.name);
+	// 		});
+	// 	}
+	// 	else {
+	// 		console.log("failed to hit the yelp api");
+	// 	}
+	// });
--- scripts.js
+++ scripts.js
@@ -1,25 +1,49 @@
 function getLocation() {
+  getEvents(39.707056800000004, -104.9838707);
 	// Try HTML5 geolocation.
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(function(position) {
-        var pos = {
-          lat: position.coords.latitude,
-          long: position.coords.longitude
-        };
-        console.log(pos.lat, pos.long);
-        getEvents(pos.lat, pos.long);
-      }, function() {
-        console.log("error getting location data.");
-      });
-    } 
-    else {
-    	console.log("Browser does not support HTML5 geolocation.");
-    }
+  // if (navigator.geolocation) {
+  //   navigator.geolocation.getCurrentPosition(function(position) {
+  //     var pos = {
+  //       lat: position.coords.latitude,
+  //       long: position.coords.longitude
+  //     };
+  //     console.log(pos.lat, pos.long);
+  //     getEvents(pos.lat, pos.long);
+  //   }, function() {
+  //     console.log("error getting location data.");
+  //   });
+  // } 
+  // else {
+  // 	console.log("Browser does not support HTML5 geolocation.");
+  // }
 }
 
 function getEvents(lat, long) {
-	let url = "/events/" + lat + "/" + long;
-	$.get("", function(data, status){
-        alert("Data: " + data + "\nStatus: " + status);
-    });
+  var data = {
+    radius: parseInt(document.getElementById("radius").value),
+    types: {
+      running: document.getElementById("running").checked,
+      climbing: document.getElementById("climbing").checked,
+      music: document.getElementById("music").checked,
+      hiking: document.getElementById("hiking").checked,
+    }
+  };
+	var url = "/events/" + lat + "/" + long;
+  $.ajax({
+    type  : "GET",
+    url   : url,
+    data  : data,
+    success: function(data, status){
+      console.log(data);
+      // data.forEach( (event) => {
+      //   renderEvent(event);
+      // });
+    }
+  });
+}
+
+function renderEvent(eventData) {
+  //event needs: _id, name, address, lat/lng, description, time, date
+  // var $div = $("<div>", {id: eventData._id, "class": "event"});
+  // $("#eventRenderLocation").append();
 }
